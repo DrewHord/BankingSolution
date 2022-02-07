6f7cc8e6b47db0cc8eb786ab05cbfbb54eba90da
Andrew Hord <drewhord@gmail.com>
2022-02-07 11:49:49 -0500

    Add project files.

---
+++ BankingProject/Account.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingProject {
+    public class Account {
+        // Static Properties
+        private static int NextAccountNumber { get; set; } = 1;
+
+        // Instance Properties
+        public decimal Balance { get; set; }
+        public int AccountNumber { get; set; }
+        public string Description { get; set; }
+        // Methods
+        public bool Deposit(decimal Amount) {
+            if(Amount <= 0) {
+                throw new Exception("Amount must be postive.");
+            }
+            Balance = Balance + Amount;
+            return true; 
+        }
+        public bool Withdraw(decimal Amount) {
+            if (Amount <= 0) {
+                throw new Exception("Amount must be postive.");
+            }
+            if (Amount > Balance) {
+                throw new Exception("Insufficient funds");
+            }
+            Balance = Balance - Amount;
+            return true; 
+        }
+        public bool Transfer(decimal Amount, Account ToAccount) {
+            try { 
+            Withdraw(Amount);
+            }catch (Exception) {
+                throw new Exception("Withdraw failed in Transfer");
+            }
+            ToAccount.Deposit(Amount);
+            return true;
+        }
+        public void Debug() {
+            Console.WriteLine($"{AccountNumber} | {Description} | {Balance:c}");
+        }
+        //Constructors
+        public Account() { // default constructor
+            AccountNumber = NextAccountNumber;
+            NextAccountNumber++;
+            Description = "New Account";
+            Balance = 0;
+        }
+
+    }
+}
